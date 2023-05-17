f3b7f0f18d3b1202736563fe44dd2e95f8217c79
asadbukharee <asadbukharee@gmail.com>
2023-05-17 18:04:12 +0500

    custom websites done

--- core.py
+++ core.py
@@ -4,7 +4,7 @@ import os
 import urllib
 
 import requests
-from bs4 import BeautifulSoup as Soup
+from bs4 import BeautifulSoup as Soup, BeautifulSoup
 from GoogleNews import GoogleNews
 
 # https://pypi.org/project/GoogleNews/
@@ -13,6 +13,73 @@ print(googlenews.getVersion())
 googlenews.enableException(True)
 
 WORDS_COUNT = 150
+NEWS_SITES = [
+    "filgoal.com",
+    "elbalad.news",
+    "skynewsarabia.com",
+    "beinsports.com",
+    "elaosboa.com",
+    "kooora.com"
+    # "goal.com"
+]
+
+
+def get_url(news):
+    for n in NEWS_SITES:
+        if news in n:
+            if not 'https://' in n:
+                url = 'https://' + n
+
+            if "filgoal" in url:
+                return url + "/ar"
+            if "goal" in url:
+                return url + "/ar"
+
+            if "elbalad" in url:
+                url = url + "/category/5"
+            if "skynewsarabia" in url:
+                url = url + "/sport"
+            if "beinsports" in url:
+                url = url + "/ar/"
+            if "elaosboa" in url:
+                url = url + "/category/sports/"
+            return url
+
+
+def get_article_urls(url):
+    article_urls = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    if "goal" in url:
+        article_urls = [link.get('href') for link in soup.find_all('a') if
+                        link.get('href') and '/ar/%' in link.get('href') and 'goal' in link.get('href')]
+
+    if "elaosboa" in url:
+        art = soup.find_all('article')
+        article_urls = [link.find('a').get('href') for link in art if link.find('a')]
+
+    if "kooora" in url:
+        art = soup.find('div', {'class': 'newsList'})
+        article_urls = [link.get('href') for link in art.find_all('a') if
+                        link.get('href')]
+
+    if "beinsports" in url:
+        article_urls = [link.get('href') for link in soup.find_all('a') if
+                        link.get('href') and '/ar/%' in link.get('href')]
+
+    if "filgoal" in url:
+        article_urls = ["https://www.filgoal.com" + link.get('href') for link in soup.find_all('a') if
+                        link.get('href') and '/articles/' in link.get('href')]
+
+    if "elbalad" in url:
+        news_list_div = soup.find('div', {'class': 'news-list'})
+        article_urls = ["https://www.elbalad.news/" + a_tag['href'] for a_tag in news_list_div.find_all('a')]
+    if "skynewsarabia" in url:
+        article_urls = ["https://www.skynewsarabia.com" + link.get('href') for link in soup.find_all('a') if
+                        link.get('href') and '/sport/' in link.get('href')]
+
+    return article_urls
 
 
 def get_details(g, url):
@@ -21,44 +88,75 @@ def get_details(g, url):
             url = 'https://' + url
 
         response = requests.get(url)
-        final_url = response.url
-        domain = final_url.split('www.')[-1].split('.')[0]
-        print("URL", final_url)
-        req = urllib.request.Request(final_url, headers=g.headers)
-        response = urllib.request.urlopen(req)
-        page = response.read()
-        content = Soup(page, "html.parser")
+        if response.status_code == 200:
+            final_url = response.url
+            domain = final_url.split('www.')[-1].split('.')[0]
+            print("URL", final_url)
+            req = urllib.request.Request(final_url, headers=g.headers)
+            response = urllib.request.urlopen(req)
+            page = response.read()
+            content = Soup(page, "html.parser")
 
-        tags = set(content.find_all())
+            tags = set(content.find_all())
 
-        # extract the names of the tags
-        article_tags = [tag.name for tag in tags if 'article' in tag.name]
-        # print(f"Article tag : {article_tags[0]}")
+            # extract the names of the tags
+            # article_tags need to be tested.
+            article_tags = [tag.name for tag in tags if 'article' in tag.name]
+            # print(f"Article tag : {article_tags[0]}")
 
-        articles = content.find(article_tags[0])
-        tags = articles.find_all(['p', 'h3'])
-        # Extract text from each tag
-        text_list = [tag.get_text() for tag in tags]  # Get the text from each <p> tag
-        article_text = "\n".join(text_list)
-        # article_body_div = content.find("div", {"id": "articleBody"})
-        # if not article_body_div:
-        #     article_body_div = content.find("div", {"id": "NewsStory"})
-        # article_text = article_body_div.get_text().strip()
-        img_data_src = []
-        try:
-            """the img tag is in article tag, and the url contains domain name"""
-            imgs = articles.find_all('img')
-            img_data_src = [im.get('data-src') for im in imgs if im.get('data-src') and domain in im.get('data-src')]
-        except:
-            print("Image not found Exception")
-        if article_text:
-            words = len(article_text.split(' '))
-            if words < WORDS_COUNT:
-                print(f"Ignoring because of less words : {words}")
-                return None
-        print("News load success")
+            if "beinsports" in url:
+                articles = content.find(article_tags[0], {'class': 'homepage'})
 
-        return {"img": img_data_src, "link": final_url, "detail": article_text}
+            articles = content.find(article_tags[0])
+            tags = articles.find_all(['p', 'h3'])
+            # Extract text from each tag
+            text_list = [tag.get_text() for tag in tags]  # Get the text from each <p> tag
+            article_text = "\n".join(text_list)
+
+            img_data_src = []
+            try:
+                """the img tag is in article tag, and the url contains domain name"""
+                imgs = articles.find_all('img')
+                if "elaosboa" in url:
+                    img_data_src = [im.get('data-src') for im in imgs if
+                                    im.get('data-src') and domain in im.get('data-src') and "uploads" in im.get(
+                                        'data-src')]
+
+                if "beinsports" in url:
+                    img_data_src = [im.get('src') for im in imgs if
+                                    im.get('src') and domain in im.get('src') and "images" in im.get('src')]
+                    if not img_data_src:
+                        img_data_src = [im.get('data-src') for im in imgs if
+                                        im.get('data-src') and domain in im.get('data-src') and "images" in im.get(
+                                            'data-src')]
+
+                if "filgoal" in url:
+                    img_data_src = [im.get('data-src') for im in imgs if
+                                    im.get('data-src') and domain in im.get('data-src') and "verylarge" in im.get(
+                                        'data-src')]
+                if "skynewsarabia" in url:
+                    img_data_src = [im.get('src') for im in imgs if
+                                    im.get('src') and domain in im.get('src') and "images/" in im.get('src')]
+
+                if "elbalad" in url:
+                    img_data_src = [im.get('srcset') for im in imgs if
+                                    im.get('srcset') and "UploadCache" in im.get('srcset')]
+                    if img_data_src:
+                        s = img_data_src[0]
+                        start = s.find("/")
+                        end = s.find(".jpeg", start) + 5
+                        img_data_src = "https://www.elbalad.news/" + s[start:end]
+
+            except:
+                print("Image not found Exception")
+            if article_text:
+                words = len(article_text.split(' '))
+                if words < WORDS_COUNT:
+                    print(f"Ignoring because of less words : {words}")
+                    return None
+            print("News load success")
+
+            return {"img": img_data_src, "link": final_url, "detail": article_text}
 
     except Exception as e:
         print(f"Ignoring because of {e}")
@@ -79,7 +177,7 @@ def website_allowed(website, news):
         return False
 
 
-def scrap_event(news):
+def scrap_event():
     try:
         # googlenews.set_lang('ar')
         googlenews.set_period('7d')
@@ -94,12 +192,12 @@ def scrap_event(news):
         for result in results:
             website = result.get('link')
             print(f"Scrapint Website: {website}")
-            if website_allowed(website, news):
-                detail = get_details(googlenews, website)
-                if detail:
-                    result.update(detail)
-                    data.append(result)
-                    # print(result)
+            # if website_allowed(website, news):
+            detail = get_details(g=googlenews, url=website)
+            if detail:
+                result.update(detail)
+                data.append(result)
+                # print(result)
         current_time = datetime.datetime.now()
         timestamp = (current_time.timestamp())
         # news = googlenews.get_texts()
@@ -108,6 +206,26 @@ def scrap_event(news):
         return str(data)
     except Exception as e:
         print(f"Google News failed: {e}")
+
+
+def scrap_custom(news):
+    results = []
+
+    for n in news:
+        print(f"{'*'*40}\n Scraping {n}")
+        url = get_url(n)
+        if url:
+            if "kooora" in url:
+                results.append("Coming soon")
+            # if "/goal" in url:
+            #     results.append("Coming soon")
+            article_urls = get_article_urls(url)
+            for i, u in enumerate(article_urls):
+                print(f"Enum => {i + 1} : {n} of {len(article_urls)}")
+                detail = get_details(g=googlenews, url=u)
+                if detail:
+                    results.append({url: detail})
+    return results
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,13 +1,53 @@
 import subprocess
-from typing import List, Optional
+from typing import List, Optional, Annotated, Union
 
-from fastapi import FastAPI,Response
+from fastapi import FastAPI, Response, Query, Depends
+from starlette.responses import StreamingResponse
 
-
-from core import scrap_event
+from core import scrap_event, scrap_custom,NEWS_SITES
 
 app = FastAPI()
 worker_process = None
+
+
+
+def parse_list(news: Optional[List[str]] = Query(None, title="News Sites",
+                                                 description=f"Select from this list of news sites  {NEWS_SITES}")) -> \
+        Optional[List]:
+    """
+    accepts strings formatted as lists with square brackets
+    names can be in the format
+    "[bob,jeff,greg]" or '["bob","jeff","greg"]'
+    """
+
+    def remove_prefix(text: str, prefix: str):
+        return text[text.startswith(prefix) and len(prefix):]
+
+    def remove_postfix(text: str, postfix: str):
+        if text.endswith(postfix):
+            text = text[:-len(postfix)]
+        return text
+
+    if news is None:
+        return
+
+    # we already have a list, we can return
+    if len(news) > 1:
+        return news
+
+    # if we don't start with a "[" and end with "]" it's just a normal entry
+    flat_names = news[0]
+    if not flat_names.startswith("[") and not flat_names.endswith("]"):
+        return news
+
+    flat_names = remove_prefix(flat_names, "[")
+    flat_names = remove_postfix(flat_names, "]")
+
+    names_list = flat_names.split(",")
+    names_list = [remove_prefix(n.strip(), "\"") for n in names_list]
+    names_list = [remove_postfix(n.strip(), "\"") for n in names_list]
+
+    return names_list
 
 
 @app.on_event('startup')
@@ -20,13 +60,27 @@ def shutdown_event():
     print("News server is shutting down")
 
 
-@app.get('/word_count')
-def get_news(count: int = 200):
+@app.get('/word-count')
+def set_word_count(count: int = 200):
     global WORDS_COUNT
     WORDS_COUNT = count
     return {"message": f"word count set to {count}", "data": []}
 
 
+@app.get('/news-sites')
+def get_news_sites():
+    global NEWS_SITES
+    return {"message": f"Available news sites: {NEWS_SITES}"}
+
+
 @app.get("/sources")
-def get_news(news: Optional[str] = None):
-    return Response(scrap_event(news), media_type="text/plain")
+def get_news(news: List[str] = Depends(parse_list)):
+    """ list param method """
+    print(news)
+    if news:
+        """scrap from the given news sources"""
+        results = scrap_custom(news)
+        return {"message": results}
+    else:
+
+        return Response(scrap_event(), media_type="text/plain")
